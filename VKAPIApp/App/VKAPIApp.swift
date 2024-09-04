0b78ac5f0ec95eb6dfd658822b16def0db5e29ec
VadimBaby <vadimdim22@gmail.com>
2024-09-05 00:03:54 +0700

    made auth

---
+++ VKAPIApp/App/VKAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKAPIAppApp.swift
+//  VKAPIApp
+//
+//  Created by Вадим Мартыненко on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct VKAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VKAPIApp/Features/PublicZone/Data/AuthCredentials.swift
@@ -0,0 +1,13 @@
+//
+//  AuthCredentials.swift
+//  VKAPIApp
+//
+//  Created by Вадим Мартыненко on 04.09.2024.
+//
+
+import Foundation
+
+final class AuthCredentials {
+    var token: String?
+    var userID: String?
+}
