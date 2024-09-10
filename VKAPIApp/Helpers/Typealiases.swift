0f96633ec75c808e8c1dc67c5076cc66dc1bf567
VadimBaby <vadimdim22@gmail.com>
2024-09-10 16:36:43 +0700

    add tabbar

---
+++ VKAPIApp/Helpers/Typealiases.swift
@@ -0,0 +1,8 @@
+//
+//  TypeAliases.swift
+//  VKAPIApp
+//
+//  Created by Вадим Мартыненко on 10.09.2024.
+//
+
+typealias VoidAction = () -> Void
---
+++ VKAPIApp/Helpers/ViewModifiers/FirstAppearModifier.swift
@@ -0,0 +1,30 @@
+//
+//  FirstAppearModifier.swift
+//  VKAPIApp
+//
+//  Created by Вадим Мартыненко on 10.09.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct FirstAppearModifier: ViewModifier {
+    
+    @State private var isFirstAppear: Bool = true
+    let action: VoidAction
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                guard isFirstAppear else { return }
+                defer { isFirstAppear = false }
+                action()
+            }
+    }
+}
+
+extension View {
+    func onFirstAppear(perform action: @escaping VoidAction) -> some View {
+        modifier(FirstAppearModifier(action: action))
+    }
+}
